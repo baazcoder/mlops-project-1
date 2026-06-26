c0e5fe436717970fec050321e4e2fad9292bab91
shehbaz <sandhushehbaz582@gmail.com>
2026-06-26 17:26:36 +0530

    project template made

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name ='src',
+    version = "0.0.1",
+    author = "shehbaz singh",
+    author_email = "sandhushehbaz582@gmail.com",
+    packages = find_packages()
+)
